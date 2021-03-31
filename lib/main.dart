4c3811453f38735c2105c415da4173b72b77bd3a
mulyana24 <49643139+mulyana24@users.noreply.github.com>
2021-03-31 17:46:02 +0700

    first update

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "Ini adalah Text";
+
+  // void pressButton() {
+  //   setState(() {
+  //     message = "Button sudah ditekan";
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+              RaisedButton(
+                child: Text("Click"),
+                onPressed: () {
+                  setState(() {
+                    message = "Button sudah ditekan";
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
